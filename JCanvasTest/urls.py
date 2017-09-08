65f305f78b9852a2d1d2062eb148bbfb2e1fa535
HoXP <hxptow@163.com>
2017-09-08 18:00:05 +0800

    Initial commit

---
+++ JCanvasTest/urls.py
@@ -0,0 +1,22 @@
+﻿"""
+Definition of urls for JCanvasTest.
+"""
+from django.conf.urls import include, url
+from Main import views as main
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'JCanvasTest.views.home', name='home'),
+    # url(r'^JCanvasTest/', include('JCanvasTest.JCanvasTest.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', main.index),
+]
