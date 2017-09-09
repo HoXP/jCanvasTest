02edc44f03fa68c19ed5a85014795e8dee2d11f9
HoXP <hoxp@github.com>
2017-09-09 15:17:21 +0800

    q

--- JCanvasTest/urls.py
+++ JCanvasTest/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', main.index),
+    url(r'^$', main.index, name='index'),
+    url(r'^sort/bubble$', main.sort_bubble, name='sort_bubble'),
 ]
--- Main/views.py
+++ Main/views.py
@@ -2,3 +2,17 @@
 
 def index(request):
     return render(request, 'index.html')
+
+def sort_bubble(request):
+    list = [34,7856,2,546,89,90,21]
+    print('开始：{}'.format(list))
+    i = 0
+    for i_item in list[:-1]:	    #遍历0至N-1的列表切片；list长度为N，因为从0开始，所以需要减1，实际进行了N-1趟循环（0~N-2）
+        j = 0
+        for j_item in list[:-1-i]:  #内层循环的要点是相邻比较。当j=4的时候，就推出循环了
+            if list[j] > list[j + 1]:   #如果相邻两个数，前者大，就交换
+                (list[j],list[j + 1]) = (list[j + 1],list[j])
+            j+=1
+        i+=1
+    print('结果：{}'.format(list))
+    return render(request, 'sort_bubble.html',{'list': list})
