dbe47df48c1406db482a9a0c45da9b4fd369eb16
HoXP <hoxp@github.com>
2017-09-29 09:13:06 +0800

    s

--- Main/static/js/learn.js
+++ Main/static/js/learn.js
@@ -1,16 +1,12 @@
-﻿var cvsId = '#cvs';
-
-function Func() {
+﻿function Func() {
     if ($(cvsId) == null)
     {
         alert("no canvas");
     }
-    alert("canvas");
     $(cvsId).drawPolygon({
         fillStyle: "#6c3",
         x: 100, y: 100,
         radius: 50, sides: 5
     });
 }
-
 $(document).ready(Func);
