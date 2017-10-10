2687f3f290270b700e5cd63d7f4138721902c3a2
HoXP <hoxp@github.com>
2017-10-10 08:08:32 +0800

    s
    tool面板随机数生成功能

--- Main/static/js/global.js
+++ Main/static/js/global.js
@@ -1,11 +1,4 @@
 ﻿var cvsId = '#cvs';
-//function ResizeCanvas() {
-//    var imageData = $(cvsId).getImageData(0, 0, $(cvsId).width, $(cvsId).height);// 保存当前图像
-//    $(cvsId).height = height;
-//    $(cvsId).width = width;
-//    $(cvsId).putImageData(imageData, 0, 0);
-//}
-//$(window).resize(ResizeCanvas);
 function Draw()
 {
     Flush();
